db7f2e7936b4ecbac7fb56af62894f66cd756b1f
Daniel Lusk <daniel.lusk@posteo.net>
2022-09-30 13:55:15 +0200

    added inference config

--- tree.py
+++ tree.py
@@ -131,6 +131,17 @@ class TreeConfig(Config):
     MINI_MASK_SHAPE = (56, 56)  # (height, width) of the mini-mask
 
 
+class TreeInferenceConfig(TreeConfig):
+    # Set batch size to 1 to run one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    # Don't resize imager for inferencing
+    IMAGE_RESIZE_MODE = "pad64"
+    # Non-max suppression threshold to filter RPN proposals.
+    # You can increase this during training to generate more propsals.
+    RPN_NMS_THRESHOLD = 0.7
+
+
 ############################################################
 #  Dataset
 ############################################################
